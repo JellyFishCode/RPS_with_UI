878ef62484695f73d539dbc7f01f88ad1dc90e09
JellyFishCode <sammi-vay@protonmail.com>
2021-03-15 16:02:09 +0100

    Add README.md, index.html, javascript.js and style.css

---
+++ README.md
@@ -0,0 +1,5 @@
+The Odin Project - Rock Paper and Scissors2 - DOM Manipulation adding UserInterface
+For the previous Project Rock Paper and Scissors we need to create a simple UserInterface where
+the player can push a button instead to type in an answer.
+
+---
---
+++ javascript.js
@@ -0,0 +1,106 @@
+const scoreID = document.querySelector("#scoreID");
+
+const scoreDisplay = document.createElement("p");
+scoreDisplay.textContent = "Spielstand:   Spieler 0  : 0 Computer";
+scoreDisplay.style.cssText = "color: #08402D;";
+scoreID.appendChild(scoreDisplay);
+
+const pickID = document.querySelector('#pickID');
+
+const playerPick = document.createElement("p")
+playerPick.textContent = "Spielerauswahl: ";
+playerPick.style.cssText = "color: #08402D;";
+pickID.appendChild(playerPick);
+
+const computerPick = document.createElement("p")
+computerPick.textContent = "Computerauswahl: ";
+computerPick.style.cssText = "color: #08402D;";
+pickID.appendChild(computerPick);
+
+let playerScore = 0;
+let computerScore = 0;
+
+var auswahlMoeglichkeiten = ["schere", "stein", "papier"];
+function computerPlay(){
+    let nummer = Math.floor(Math.random() * auswahlMoeglichkeiten.length);
+    switch(nummer){
+        case 0:
+            return "SCHERE";
+        case 1:
+            return "STEIN";
+        case 2:
+            return "PAPIER";
+    }
+}
+
+function playRound(playerChoice){
+    let spielerAuswahl = playerChoice;
+    let computerAuswahl = computerPlay();
+    playerPick.textContent = "Spielerauswahl: " + playerChoice;
+    computerPick.textContent = "Computerauswahl: " + computerAuswahl;
+
+    if(spielerAuswahl === computerAuswahl){
+    console.log("Unentschieden");
+    }
+    if(
+        (spielerAuswahl === "STEIN" && computerAuswahl === "SCHERE") ||
+        (spielerAuswahl === "SCHERE" && computerAuswahl === "PAPIER") ||
+        (spielerAuswahl === "PAPIER" && computerAuswahl === "STEIN")
+    ){
+        playerScore += 1;
+        scoreDisplay.textContent = "Spielstand:   Spieler " + playerScore + " : " + computerScore + " Computer";
+        console.log("Du gewinnst die Runde");
+    }
+    if(
+        (computerAuswahl === "STEIN" && spielerAuswahl === "SCHERE") ||
+        (computerAuswahl === "SCHERE" && spielerAuswahl === "PAPIER") ||
+        (computerAuswahl === "PAPIER" && spielerAuswahl === "STEIN")
+    ){
+        computerScore += 1;
+        scoreDisplay.textContent = "Spielstand:   Spieler " + playerScore + " : " + computerScore + " Computer";
+        console.log("Du verlierst die Runde");
+    }  
+    if(playerScore === 5 || computerScore == 5){
+        if(playerScore > computerScore){
+            window.alert("Herzlichen Glückwunsch! Du hast gewonnen.");
+        }
+        else{
+            window.alert("Du hast verloren. Viel Glück beim nächsten Mal.");
+        }
+        playerScore = 0;
+        computerScore = 0;
+        scoreDisplay.textContent = "Spielstand:   Spieler " + playerScore + " : " + computerScore + " Computer";
+    }        
+
+}
+
+const buttonScissors = document.querySelector("#scissors");
+buttonScissors.addEventListener('click', () => {
+    playRound("SCHERE");
+});
+const buttonStone = document.querySelector("#stone");
+buttonStone.addEventListener('click', () => {
+    playRound("STEIN");
+});
+const buttonPaper = document.querySelector("#paper");
+buttonPaper.addEventListener('click', () => {
+    playRound("PAPIER");
+});
+
+// Additional Stuff
+const btn = document.querySelector("#btn");
+btn.addEventListener('click', function(e){
+    e.target.style.background = 'white';
+});
+
+btn.addEventListener('mouseenter', () => {
+    btn.textContent = "Your mouse's inside this Button";
+});
+btn.addEventListener('mouseout', () => {
+    btn.textContent = "Event Button";
+});
+
+const copy = document.querySelector("#copy");
+copy.addEventListener('copy', function(e){
+    window.alert("Do not copy this title... for whatever reason.");
+});
